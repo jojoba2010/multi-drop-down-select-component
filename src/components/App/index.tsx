2d57bce4e7e6d7f78db99df0bcf1e3789d4b9fb0
alin <alin@snapwork.com>
2025-03-13 16:06:12 +0330

    My first commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ShowDropdown from "../../pages/showDropdown";
+import * as styles from "./index.scss";
+function App() {
+  return (
+    <div className={styles["main-container"]}>
+      <ShowDropdown />
+    </div>
+  );
+}
+export default App;
---
+++ src/pages/showDropdown/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import Ball from '../../assets/ball.png'
+import Gamepad from '../../assets/gamepad.png'
+import Education from '../../assets/education.png'
+import Science from '../../assets/science.png'
+import Art from '../../assets/art.png'
+import Health from '../../assets/health.png'
+import Dropdown from '../../components/UI/dropdown'
+interface Item {
+  label: string;
+  value: string|number;
+  icon?: string;
+}
+const ShowDropdown=()=> {
+  const items=[{
+    label: 'Education',
+    value: 'education',
+    icon: Education
+  },
+  {
+    label: 'Science',
+    value: 'science',
+    icon: Science
+  },
+  {
+    label: 'Art',
+    value: 'art',
+    icon: Art
+  },
+  {
+    label: 'Sport',
+    value: 'sport',
+    icon: Ball
+  },
+  {
+    label: 'Games',
+    value: 'games',
+    icon: Gamepad
+  },
+  {
+    label: 'Health',
+    value: 'Health',
+    icon: Health
+  }]
+  const onChangeDropdown=(slectedValues:Item[])=>{
+    console.log('slectedValues##', slectedValues)
+  }
+  const onNewItemAdded=(newItem:Item, allItems:Item[])=>{
+    console.log('newItem##',newItem,'All items##' ,allItems)
+  }
+  return (<Dropdown 
+          items={items} 
+          onChange={onChangeDropdown} 
+          onNewItemAdded={onNewItemAdded}   
+          defaultValue={[{
+            label: 'Games',
+            value: 'games',
+          },
+          {
+            label: 'Health',
+            value: 'Health',
+          }]}
+          /> );
+}
+
+export default ShowDropdown;
